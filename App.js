99b1a2691d97d9689d72ed7a948ad729c4e420d2
Caio <cmsssilvva@id.uff.br>
2022-02-21 11:40:59 -0300

    componentes prontos sem estilização

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView>
+      {/* View para iamgem da logo */}
+      <View>
+        <Image 
+          source={require('./assets/logo-civa.png')}
+        />
+      </View>
+
+      {/* View para entradas */}
+      <View>
+        <TextInput 
+          placeholder="Email"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+      </View>
+      <View>
+        <TextInput 
+          placeholder="Senha"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Text>Login</Text>
+      </TouchableOpacity>
+      <Text>Não possui uma conta?</Text><TouchableOpacity><Text>Crie uma agora</Text></TouchableOpacity>
+    </KeyboardAvoidingView>
   );
 }
 
